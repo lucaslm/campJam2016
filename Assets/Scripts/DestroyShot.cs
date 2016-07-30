b81a2da490005c9c759dc11d4fa788c83569d5b5
VFerrari <vferrari@mpc.com.br>
2016-07-30 06:36:39 -0300

    Adding fully functional projectiles

---
+++ Assets/Scripts/DestroyShot.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyShot : MonoBehaviour {
+
+	public float fail = 5.0f;
+	float time;
+
+	//When collided.
+	void OnTriggerEnter2D(Collider2D coll){
+		
+		if(coll.name == "asteroid Prefab(Clone)"){
+			Destroy (gameObject);
+			Destroy (GameObject.FindGameObjectWithTag ("Enemy"));
+		}
+	}
+
+
+	//Failsafe
+	void Update (){
+
+		if (Input.GetKey (KeyCode.Space)) {
+			time=0;
+		}
+
+		time+=Time.deltaTime;
+		if (time>=fail){
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject shot;
+	public float cooldown = 1.0f;
+	float time;
+	bool cantshoot;
+
+	// Use this for initialization
+	void Start () {
+		time = 0;
+		cantshoot = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!cantshoot) {
+			if (Input.GetKey (KeyCode.Space)) {
+				Instantiate (shot, transform.position, Quaternion.identity);
+				time = 0;
+				cantshoot = true;
+			}
+		}
+
+
+		//Cooldown
+		time+=Time.deltaTime;
+		if (time >= cooldown) {
+			cantshoot = false;
+		}
+	}
+}
